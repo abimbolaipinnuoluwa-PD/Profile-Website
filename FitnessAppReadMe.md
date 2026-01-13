fc5e6dcfd0da0cff368b85583c60e5be5ee6b499
Ipinnuoluwa Abimbola <ebenezer@relianceinfosystems.com>
2026-01-13 08:46:50 +0100

    update 4th assignment done: creating fitapp

---
+++ FitnessAppReadMe.md
@@ -0,0 +1,22 @@
+# Fitness-app/
+
+├── index.html
+├── about.html
+|──-- Dashboard.html
+├── style.css
+├── about.css
+├── script.js
+|__<link rel="stylesheet" href="fitnessappreadme.md">
+|__[text](https://ReadMe.md)
+
+This FitApp is a simple fitness app for tracking goals
+
+- Onboarding flow
+- About page
+- Menu
+- Contact
+
+Description:
+Stay fit with quick workouts, local foods and holiday challenges!
+
+Category: Health & Fitness.
---
+++ script.js
@@ -0,0 +1,28 @@
+// script.js
+function askGoals() {
+    alert("Let us set your fitness goals!")
+    // Next step: Tour Features
+    window.location ='dashboard.html';
+}
+
+function ShowTour() {
+    alert("Tour: Explore workouts, diets and track progress with notes!")
+    // Next step: Tour Features
+}
+
+function ExploreMenu() {
+    alert("Menu: Explore parenting controlled social medial life, diets on global standards and track progress with notes!")
+    // Next step: Tour Features
+}
+
+function showLogin() {
+    let details = prompt("Enter Full Name, Email, Contact/Phone Number and Password, Age, Sex/Gender, Date of Birth (DOB), Location, Preference, Promo code if any, Comments!")
+    // Process login/Details
+    alert("Details saved and submitted successfully!")
+}
+
+function showHolidayChallenges() {
+    alert("Get ready for FitApp's Holiday Challenges! Workouts for busy Nigerians")
+    // Process login/Details
+    
+}
